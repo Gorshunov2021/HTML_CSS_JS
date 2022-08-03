d583973798767410c5b8c43c9c2ca7af02287d13
Gorshunov2021 <gorshunov2.20@gmail.com>
2022-08-03 22:18:29 +0300

    03.08.2022

--- task0021_JS_News_light_and_dark/script.js
+++ task0021_JS_News_light_and_dark/script.js
@@ -7,3 +7,14 @@ themeButton.onclick = function() {
 	page.classList.toggle('light-theme');
    page.classList.toggle('dark-theme');
 };
+
+let message=document.querySelector('.subscription-message');
+
+let form=document.querySelector('.subscription');
+
+let email = document.querySelector('.subscription-email');
+
+form.onsubmit=function(evt) {
+evt.preventDefault();
+message.textContent='Адрес ' + email.value + ' добавлен в список получателей рассылки.'
+};

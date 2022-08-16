eae50d89063a84f66367c5c035cac60daa776c8c
Gorshunov2021 <gorshunov2.20@gmail.com>
2022-08-16 23:11:17 +0300

    16.08.2022

---
+++ task0030_alert_prompt_confirm/prompt.js
@@ -0,0 +1,17 @@
+'use strict';
+//alert
+alert("Hello World !");
+
+//prompt
+//result=prompt(title, [default]);
+let age=prompt("Сколько тебе лет?", 200);
+alert(`Тебе ${age} лет ? Ты чё ???`);
+
+//confirm
+//result=confirm(question);
+
+let isBoss=confirm("Ты здесь главный");
+alert(isBoss);
+
+let name=prompt("Как тебя зовут???", "");
+alert(`Тебя зовут ${name} !!!`);

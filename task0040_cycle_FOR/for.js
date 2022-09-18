d7c8b4f67fbae4417dacbc22f26044514eef80f3
Gorshunov2021 <gorshunov2.20@gmail.com>
2022-09-18 11:44:19 +0300

    18.09.2022

---
+++ task0040_cycle_FOR/for.js
@@ -0,0 +1,23 @@
+for (let i=0; i<4; i++) {
+	alert(i);
+}
+
+for (let x=2; x<10; x++) alert(x)
+
+let s=0;
+for (s=0; s<2; s++) {
+	alert (s);
+}
+alert (s);
+
+//Можно делать и так!!!
+
+let e=0;
+for (; e<3; e++){
+	alert (e);
+}
+
+let Q=0;
+for (; Q<5;) {
+	alert (Q++);
+}

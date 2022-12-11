68a4a10d2ab79c52c4518c6b5367ead9b4bbfe2c
Gorshunov2021 <gorshunov2.20@gmail.com>
2022-12-11 16:55:54 +0300

    11.12.2022

---
+++ task0047_Onclick_Function_js/Onclick.js
@@ -0,0 +1,19 @@
+function MyFunction1 () {
+	const L = "1166"
+	const B = "598"
+	const S = "0,5"
+
+	document.getElementById("demo1").innerHTML = L + " x " + B + " x " + S;
+	document.getElementById("demo2").innerHTML = B;
+	document.getElementById("demo3").innerHTML = S;
+}
+
+function MyFunction2 () {
+	const L = "1833"
+	const B = "620"
+	const S = "0,7"
+
+	document.getElementById("demo4").innerHTML = L + " x " + B + " x " + S;
+	document.getElementById("demo4").style.color = "red";
+}
+
